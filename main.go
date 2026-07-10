5059fba06599536b14aaa80c7bd71d3748cacd6f
InvaderJT <invaderjt@gmail.com>
2026-07-10 02:12:41 -0400

    completed ch1

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
-var Mux = http.NewServeMux()
+func main() {
+	const port = "8080"
+	const filepathRoot = "."
 
-var Server = http.Server{
-	Addr:    ":8080",
-	Handler: Mux,
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handleHealthz)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Fatal(server.ListenAndServe())
+
 }
 
-func main() {
-	Server.ListenAndServe()
+func handleHealthz(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	_, err := writer.Write([]byte("OK"))
+	if err != nil {
+		log.Println(err)
+	}
 }
